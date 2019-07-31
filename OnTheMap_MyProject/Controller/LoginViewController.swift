fbe8afd60cf9384ae1846f4da0c4d49589b630f0
Pranay Boggarapu <Pranay.Boggarapu@macys.com>
2019-07-31 00:46:22 -0700

    Completed the Login Page UI

--- OnTheMap_MyProject/Controller/LoginViewController.swift
+++ OnTheMap_MyProject/Controller/LoginViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var signUpCompleteLabel: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loginButton.layer.cornerRadius = 5
+        setupSignUpLink()
         // Do any additional setup after loading the view, typically from a nib.
     }
+}
 
-
+extension LoginViewController: UITextViewDelegate {
+    
+    func setupSignUpLink() {
+        let attributedString = NSMutableAttributedString(string: "Don't have an account? Sign Up")
+        attributedString.addAttribute(.link, value: "https://www.hackingwithswift.com", range: NSRange(location: 23, length: 7))
+        
+        
+        signUpCompleteLabel.attributedText = attributedString
+    }
+    
+    func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+        UIApplication.shared.open(URL)
+        return false
+    }
+    
 }
 
