1020b668a13b995c6e8c98892597dfb2b8b2ed86
Pranay Boggarapu <Pranay.Boggarapu@macys.com>
2019-07-30 22:08:50 -0700

    Added the images, Implemented the launch screen

---
+++ OnTheMap_MyProject/Controller/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  OnTheMap_MyProject
+//
+//  Created by Pranay Boggarapu on 7/30/19.
+//  Copyright © 2019 Pranay Boggarapu. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
